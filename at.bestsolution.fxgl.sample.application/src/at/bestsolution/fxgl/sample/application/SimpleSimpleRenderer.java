b6b729f01cc543c0be7b4598d9ee30f9f395d982
Christoph Caks <ccaks@bestsolution.at>
2018-09-23 02:38:26 +0200

    refs #1: ported GL33Renderer to new sample infrastructure

---
+++ at.bestsolution.fxgl.sample.application/src/at/bestsolution/fxgl/sample/application/SimpleSimpleRenderer.java
@@ -0,0 +1,53 @@
+package at.bestsolution.fxgl.sample.application;
+
+import at.bestsolution.fxgl.es2.GLSurface;
+import at.bestsolution.fxgl.sample.renderer.simple.SimpleRenderer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class SimpleSimpleRenderer extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		GLSurface surface = new GLSurface();
+		
+		SimpleRenderer renderer = new SimpleRenderer();
+		renderer.setRenderTarget(surface);
+		
+		BorderPane layout = new BorderPane();
+		
+		ToolBar tb = new ToolBar();
+		{
+			Button b = new Button("Start");
+			b.setOnAction(e -> renderer.start());
+			tb.getItems().add(b);
+		}
+		{
+			Button b = new Button("Stop");
+			b.setOnAction(e -> renderer.stop());
+			tb.getItems().add(b);
+		}
+		
+		layout.setTop(tb);
+		
+		layout.setCenter(surface);
+		layout.setPrefSize(500, 300);
+		
+		Scene scen = new Scene(layout);
+		
+		primaryStage.setScene(scen);
+		primaryStage.setTitle("SimpleObjRenderer");
+		primaryStage.show();
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
---
+++ at.bestsolution.fxgl.sample.renderer.simple/src/at/bestsolution/fxgl/sample/renderer/simple/SimpleRenderer.java
@@ -0,0 +1,35 @@
+package at.bestsolution.fxgl.sample.renderer.simple;
+
+
+import at.bestsolution.fxgl.sample.renderer.Renderer;
+
+public class SimpleRenderer extends Renderer {
+
+	static {
+		Library.require();
+	}
+	
+	private long rendererId;
+	
+	public SimpleRenderer() {
+		rendererId = nCreate();
+	}
+	
+	@Override
+	public long getRendererId() {
+		return rendererId;
+	}
+	
+	public void dispose() {
+		nDestroy(rendererId);
+	}
+	
+	public void LoadObj(String name) {
+		nLoadObj(rendererId, name);
+	}
+
+	private static native long nCreate();
+	private static native void nDestroy(long rendererId);
+	private static native void nLoadObj(long rendererId, String name);
+	
+}
