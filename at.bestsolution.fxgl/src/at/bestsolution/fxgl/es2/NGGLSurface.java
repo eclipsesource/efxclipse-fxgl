7672bc6d33ec11476a419902a13d36e2a1b0c2c4
Christoph Caks <ccaks@bestsolution.at>
2018-10-02 16:49:08 +0200

    refs #1: added code to clear texture after creation

--- at.bestsolution.fxgl/src/at/bestsolution/fxgl/es2/NGGLSurface.java
+++ at.bestsolution.fxgl/src/at/bestsolution/fxgl/es2/NGGLSurface.java
@@ -11,6 +11,7 @@ import com.sun.javafx.sg.prism.NGGroup;
 import com.sun.javafx.sg.prism.NGNode;
 import com.sun.javafx.text.TextRun;
 import com.sun.prism.Graphics;
+import com.sun.prism.Image;
 import com.sun.prism.PixelFormat;
 import com.sun.prism.RTTexture;
 import com.sun.prism.ResourceFactory;
@@ -58,13 +59,24 @@ public class NGGLSurface extends NGNode {
 		
 	}
 	
+	private void clearTex(Tex tex) {
+		Image img = Image.fromByteBgraPreData(new byte[tex.w * tex.h * 4], tex.w, tex.h);
+		tex.fxTex.update(img, 0, 0);
+	}
 	
 	private Tex createTex(int width, int height) {
 //		System.err.println("Java createTex(" + width + ", " + height + ") @ " + Thread.currentThread().getName());
 		Tex tex = new Tex();
 		tex.w = width;
 		tex.h = height;
-		tex.fxTex = resourceFactory.createTexture(PixelFormat.BYTE_RGB, Texture.Usage.DYNAMIC, Texture.WrapMode.CLAMP_NOT_NEEDED, width, height);
+		tex.fxTex = resourceFactory.createTexture(PixelFormat.BYTE_BGRA_PRE, Texture.Usage.DYNAMIC, Texture.WrapMode.CLAMP_NOT_NEEDED, width, height);
+		
+		// on some hardware the texture is not empty, it contains some leftovers from the graphics memory
+		// to prevent artifacts before the native renderer clears the texture the first time
+		// we need to initialize the texture with empty pixels
+		clearTex(tex);
+		
+		
 		if (tex.fxTex == null) {
 			System.err.println("WE FAILED BIG TIME!!");
 		}
